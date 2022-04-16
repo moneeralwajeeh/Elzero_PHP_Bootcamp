c8349d05b46f3c203fc18cd4118838287dd3c442
moneeralwajeeh <alwajeehmoneer@hotmail.com>
2022-04-16 14:44:17 +0800

    First Assignment 1-5

---
+++ PHP_1-5.php
@@ -0,0 +1,41 @@
+
+<?php
+/** First Assignment */
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="<?php print 'UTF-8'; ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo 'My First PHP Page';?></title>
+  </head>
+  <body>
+      
+    <div><?php print'We Love ';?></div>
+    <div><?php print' Elzero Channel';?></div>
+  </body>
+</html>
+
+<?php
+# Second Assignment
+
+//echo 'Prevent Me From Running Please';
+#echo 'Prevent Me From Running Please';
+/*echo 'Prevent Me From Running Please';*/
+
+# Third Assignment
+
+/* My Application
+ Version 1.0
+ Created By Elzero */
+
+
+# Forth Assignment
+
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment
+
+// All the comments are Correct
